be2419d0dcc8ae9304724ca286632cb0b461cb9c
Stefan Petrus Botes <25985116@sun.ac.za>
2025-07-22 22:31:53 +0200

    Added basic ESP32 communication over websocket. Display data from ESP32 and send Turn LED ON/OFF commands.

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,25 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
+import 'package:web_socket_channel/web_socket_channel.dart'; //Use for WebSocket
+
+bool Fan1_LED = true; //Global variable to store LED state
 
 void main() {
-  runApp(MyApp());
+  //runApp(MyApp());
+  runApp(Holo3D());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class Holo3D extends StatelessWidget {
+  const Holo3D({super.key});
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'Holo3D',
         theme: ThemeData(
           useMaterial3: true,
           colorScheme: ColorScheme.fromSeed(
@@ -30,7 +34,7 @@ class MyApp extends StatelessWidget {
 
 //Methods
 class MyAppState extends ChangeNotifier {
-final ScrollController _scrollController = ScrollController();
+  final ScrollController _scrollController = ScrollController();
 
 // Function to scroll to the bottom
   void _scrollToBottom() {
@@ -39,7 +43,7 @@ final ScrollController _scrollController = ScrollController();
       duration: Duration(milliseconds: 300),
       curve: Curves.easeOut,
     );
-  }    
+  }
 
   var current = WordPair.random();
   var allWordPairs = <WordPair>[];
@@ -75,6 +79,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+//Navigation Rail
 class _MyHomePageState extends State<MyHomePage> {
   var selectedIndex = 0;
 
@@ -84,50 +89,120 @@ class _MyHomePageState extends State<MyHomePage> {
     switch (selectedIndex) {
       case 0:
         page = GeneratorPage();
-        
+
       case 1:
         page = FavouritesPage();
-        
+
+      case 2:
+        page = Fan1();
+
       default:
         throw UnimplementedError('no widget for $selectedIndex');
     }
 
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          body: Row(
-            children: [
-              SafeArea(
-                child: NavigationRail(
-                  extended: constraints.maxWidth >= 600, //Show Label if enough space ex. Rotated
-                  destinations: [
-                    NavigationRailDestination(
-                      icon: Icon(Icons.home),
-                      label: Text('Home'),
-                    ),
-                    NavigationRailDestination(
-                      icon: Icon(Icons.favorite),
-                      label: Text('Favorites'),
-                    ),
-                  ],
-                  selectedIndex: selectedIndex,
-                  onDestinationSelected: (value) {
-                    setState(() {
-                      selectedIndex = value;
-                    });
-                  },
-                ),
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        body: Row(
+          children: [
+            SafeArea(
+              child: NavigationRail(
+                extended: constraints.maxWidth >=
+                    600, //Show Label if enough space ex. Rotated
+                destinations: [
+                  NavigationRailDestination(
+                    icon: Icon(Icons.home),
+                    label: Text('Home'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Icons.favorite),
+                    label: Text('Favorites'),
+                  ),
+                  NavigationRailDestination(
+                    icon: Icon(Symbols.mode_fan),
+                    label: Text('Fan1'),
+                  ),
+                ],
+                selectedIndex: selectedIndex,
+                onDestinationSelected: (value) {
+                  setState(() {
+                    selectedIndex = value;
+                  });
+                },
               ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
-                ),
+            ),
+            Expanded(
+              child: Container(
+                color: Theme.of(context).colorScheme.primaryContainer,
+                child: page,
               ),
-            ],
-          ),
-        );
-      }
+            ),
+          ],
+        ),
+      );
+    });
+  }
+}
+
+class Fan1 extends StatefulWidget {
+  const Fan1({Key? key}) : super(key: key);
+
+  @override
+  State<Fan1> createState() => _Fan1State();
+}
+
+class _Fan1State extends State<Fan1> {
+  //Connect to WebSocket
+  late WebSocketChannel channel;
+
+  @override
+  void initState() {
+    print('Initstate start');
+    super.initState();
+    channel = WebSocketChannel.connect(
+      Uri.parse('ws://192.168.4.1:80/ws'),
+    );
+    print('Initstate end');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print('Build start');
+    final theme = Theme.of(context); //Get global theme
+
+    final style = theme.textTheme.titleLarge!.copyWith(
+      color: theme.colorScheme.primary, //Set text style color
+      fontWeight: FontWeight.bold,
+    );
+
+    print('Scaffold start');
+    return Scaffold(
+      body: Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+        StreamBuilder(
+          stream: channel.stream,
+          builder: (context, snapshot) {
+          return Text(snapshot.hasData ? '${snapshot.data}' : '');
+          },
+        ),
+        SizedBox(height: 20),
+        ElevatedButton(
+          onPressed: () {
+          channel.sink.add('LED_ON');
+          },
+          child: Text('Turn LED ON'),
+        ),
+        SizedBox(height: 10),
+        ElevatedButton(
+          onPressed: () {
+          channel.sink.add('LED_OFF');
+          },
+          child: Text('Turn LED OFF'),
+        ),
+        ],
+      ),
+      ),
     );
   }
 }
@@ -142,20 +217,26 @@ class FavouritesPage extends StatelessWidget {
       fontWeight: FontWeight.bold,
     );
 
-    var appState = context.watch<MyAppState>();   
+    var appState = context.watch<MyAppState>();
 
     return Scaffold(
         body: ListView(
-          
       children: [
         Row(
           children: [
-            Icon(Icons.favorite, color: theme.primaryColor,),
-            SizedBox(width: 10,),
-            Text('You have ${appState.fav.length} favourites:', style: style,),
+            Icon(
+              Icons.favorite,
+              color: theme.primaryColor,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              'You have ${appState.fav.length} favourites:',
+              style: style,
+            ),
           ],
         ),
-
         for (var msg in appState.fav)
           ListTile(
             title: Text(msg.asPascalCase),
@@ -168,8 +249,6 @@ class FavouritesPage extends StatelessWidget {
 class GeneratorPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
-
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
 
@@ -181,31 +260,28 @@ class GeneratorPage extends StatelessWidget {
       likeIcon = Icons.favorite_outline_outlined;
     }
 
-    
-
     return Scaffold(
-      body:     
-       Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        
-         children: [
+        children: [
           SizedBox(
-            
-              height: 150,
+            height: 150,
             child: ListView(
               controller: appState._scrollController,
               children: [
                 for (var msg in appState.allWordPairs)
-                if (appState.fav.contains(msg))
-                ListTile(title: Text(msg.asPascalCase), leading: Icon(Icons.favorite),)
-                else ListTile(title: Text(msg.asPascalCase))
+                  if (appState.fav.contains(msg))
+                    ListTile(
+                      title: Text(msg.asPascalCase),
+                      leading: Icon(Icons.favorite),
+                    )
+                  else
+                    ListTile(title: Text(msg.asPascalCase))
               ],
-            ),   
-            ), 
-
-           Center(        
-            child: 
-            Column(
+            ),
+          ),
+          Center(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 BigCard(pair: pair),
@@ -220,9 +296,9 @@ class GeneratorPage extends StatelessWidget {
                       },
                       child: Text('Next'),
                     ),
-           
+
                     SizedBox(width: 20), //Some space
-           
+
                     //Like Button
                     ElevatedButton.icon(
                       onPressed: appState.toggleFavourite,
@@ -233,13 +309,12 @@ class GeneratorPage extends StatelessWidget {
                 ),
               ],
             ),
-                 ),
-         ],
-       ),
+          ),
+        ],
+      ),
     );
   }
 }
-
 
 class BigCard extends StatelessWidget {
   const BigCard({
